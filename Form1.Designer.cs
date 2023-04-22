571696448ceb2e55a2ffe3bb8e91ba7c14c6eab3
AlinFlorescu80 <117597206+AlinFlorescu80@users.noreply.github.com>
2023-04-22 03:42:43 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ConversieAMasurilor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tipulDeMasuraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lungimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greutateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temperaturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.tipulDeMasuraToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1405, 48);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(71, 36);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tipulDeMasuraToolStripMenuItem
+            // 
+            this.tipulDeMasuraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lungimeToolStripMenuItem,
+            this.greutateToolStripMenuItem,
+            this.temperaturaToolStripMenuItem});
+            this.tipulDeMasuraToolStripMenuItem.Name = "tipulDeMasuraToolStripMenuItem";
+            this.tipulDeMasuraToolStripMenuItem.Size = new System.Drawing.Size(206, 36);
+            this.tipulDeMasuraToolStripMenuItem.Text = "Tipul de Masura";
+            // 
+            // lungimeToolStripMenuItem
+            // 
+            this.lungimeToolStripMenuItem.Name = "lungimeToolStripMenuItem";
+            this.lungimeToolStripMenuItem.Size = new System.Drawing.Size(281, 44);
+            this.lungimeToolStripMenuItem.Text = "Lungime";
+            this.lungimeToolStripMenuItem.Click += new System.EventHandler(this.lungimeToolStripMenuItem_Click);
+            // 
+            // greutateToolStripMenuItem
+            // 
+            this.greutateToolStripMenuItem.Name = "greutateToolStripMenuItem";
+            this.greutateToolStripMenuItem.Size = new System.Drawing.Size(281, 44);
+            this.greutateToolStripMenuItem.Text = "Greutate";
+            this.greutateToolStripMenuItem.Click += new System.EventHandler(this.greutateToolStripMenuItem_Click);
+            // 
+            // temperaturaToolStripMenuItem
+            // 
+            this.temperaturaToolStripMenuItem.Name = "temperaturaToolStripMenuItem";
+            this.temperaturaToolStripMenuItem.Size = new System.Drawing.Size(281, 44);
+            this.temperaturaToolStripMenuItem.Text = "Temperatura";
+            this.temperaturaToolStripMenuItem.Click += new System.EventHandler(this.temperaturaToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(149, 130);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(251, 33);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Alegeti unitatea input";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 4;
+            this.numericUpDown1.Location = new System.Drawing.Point(208, 249);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(870, 130);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(288, 33);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "Alegeti unitatea output";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1066, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(540, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Alegeti tipul de Masura";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(545, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 68);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculeaza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(870, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rezultat:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1405, 781);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStripMenuItem tipulDeMasuraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lungimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greutateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem temperaturaToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ConversieAMasurilor
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<string, double> rateDeConversie = new Dictionary<string, double>()
+    {
+        { "CelsiusFahrenheit", 1.8 },
+        { "CelsiusKelvin", 1 },
+
+        { "FahrenheitCelsius", 0.555 },
+        { "FahrenheitKelvin", 0.555 },
+
+        { "KelvinCelsius", 1 },
+        { "KelvinFahrenheit", 1.8 },
+
+            {"GrameUncii", 0.035},
+            {"GrameKilograme", 0.001 },
+            {"GrameLivre", 0.0022 },
+
+            {"KilogrameGrame", 1000 },
+            {"KilogrameLivre", 2.205 },
+            {"KilogrameUncii", 35.274 },
+
+            {"UnciiGrame", 28.35 },
+            {"UnciiLivre", 0.0625 },
+            {"UnciiKilograme", 0.0283 },
+
+            {"LivreUncii", 16 },
+            {"LivreKilograme", 0.453 },
+            {"LivreGrame", 453.592 },
+
+        { "MetriiInch", 39.37 },
+        { "MetriiFeet", 3.281 },
+        { "MetriiKilometrii", 0.001 },
+
+        { "InchMetrii", 0.0254 },
+        { "InchFeet", 0.0833 },
+        { "InchKilometrii", 0.0000254 },
+
+        { "FeetMetrii", 0.305 },
+        { "FeetInch", 12 },
+        { "FeetKilometrii", 0.0003048 },
+
+        { "KilometriiMetrii", 1000 },
+        { "KilometriiInch", 39370.1 },
+        { "KilometriiFeet", 3280.84 }
+
+    };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult Exit = MessageBox.Show("Sigur doriti sa iesiti?", "Exit", MessageBoxButtons.YesNo);
+            if (Exit == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application .Exit();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void greutateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Greutate";
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(new object[] {"Kilograme","Grame","Uncii","Livre"});
+
+            comboBox2.Items.Clear();
+            comboBox2.Items.AddRange(new object[] { "Kilograme", "Grame", "Uncii", "Livre" });
+        }
+
+        private void lungimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Lungime";
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(new object[] { "Metrii", "Inch", "Feet", "Kilometrii" });
+
+            comboBox2.Items.Clear();
+            comboBox2.Items.AddRange(new object[] { "Metrii", "Inch", "Feet", "Kilometrii" });
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void temperaturaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Temperatura";
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(new object[] { "Celsius", "Fahrenheit", "Kelvin"});
+
+
+            comboBox2.Items.Clear();
+            comboBox2.Items.AddRange(new object[] { "Celsius", "Fahrenheit", "Kelvin" });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string unitatiAlese = comboBox1.Text + comboBox2.Text;
+            try
+            {
+                double rataDeConversie = rateDeConversie[unitatiAlese];
+                double rezultat = (Convert.ToDouble(numericUpDown1.Value) * rataDeConversie) + ((comboBox2.Text == "Fahrenheit") ? 32 : 0 + ((comboBox2.Text == "Kelvin") ? -273.15 : 0));
+                label1.Text = rezultat.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Alegeti unitati de masura. Acestea trebuie sa fie diferite una fata de cealalta", "Eroare", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
